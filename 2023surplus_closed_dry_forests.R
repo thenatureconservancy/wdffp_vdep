14373df3133f7019dbe3d4285befa7e792c25892
Randy Swaty <39503604+rswaty@users.noreply.github.com>
2025-07-10 14:41:09 -0400

    working up calculations to include hectares

--- 2023surplus_closed_dry_forests.R
+++ 2023surplus_closed_dry_forests.R
@@ -17,14 +17,26 @@ closed_rows_2023 <- data_2023_dry |>
   filter(grepl("CLS", StateClassID)) |>
   filter(GROUPVEG %in% c("Hardwood", "Conifer", "Hardwood-Conifer")) |>
   mutate(ref_count = (ref_percent * total_count_pa_bps)/100) |>
-  mutate(surplus_deficit_hectares = (count_pa_bps_scls_2023 - ref_count) * 0.09)
+  mutate(ref_hectares = (ref_count * 0.09)) |>
+  mutate(current_hectares = (count_pa_bps_scls_2023 * 0.09)) |>
+  mutate(surplus_deficit_hectares = (current_hectares - ref_hectares))
 
 
-write_csv(closed_rows_2023, file = "outputs/dry_forests_closed_rows_2023.csv")
 
 cls_hectares_difference_per_pa_2023 <- closed_rows_2023 |>
   group_by(pas) |>
-  summarize(forests_closed_hectares_difference_2023 = sum(surplus_deficit_hectares))
+  summarize(
+    ref_closed_hectares = sum(ref_hectares),
+    current_closed_hectares = sum(current_hectares)) |>
+  mutate(surplus_deficit_hectares = (current_closed_hectares - ref_closed_hectares)) |>
+  mutate(percent_change = ((current_closed_hectares - ref_closed_hectares) / ref_closed_hectares) * 100)
+
+
+
+
+
+
+write_csv(closed_rows_2023, file = "outputs/dry_forests_closed_rows_2023.csv")
 
 write_csv(cls_hectares_difference_per_pa_2023, file = "outputs/dry_forests_cls_acres_difference_per_pa_2023.csv" )  
 
